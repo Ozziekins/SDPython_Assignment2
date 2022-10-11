7bf83a21dc5fd5f6efdecc6cb430eb8d614b363b
CodeSmith <emiolaabdulmueez@gmail.com>
2022-10-11 21:44:14 +0300

    Storing fetched data to a postgressql file

---
+++ src/Task 2/SqlProvider.py
@@ -0,0 +1,26 @@
+import psycopg2
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class SqlProvider:
+    _instance = None
+    engine = create_engine('postgresql://postgres:example@127.0.0.1/app', echo=True)
+    connection = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(SqlProvider, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        if self.connection is None:
+            self.connection = self.engine.connect()
+
+    def execute(self, query):
+        self.cursor.execute(query)
+
+    def create_tables(self):
+        Base.metadata.create_all(self.engine)
--- src/Task 2/test.py
+++ src/Task 2/test.py
@@ -2,13 +2,17 @@ from datetime import datetime
 import time
 from Files import Files
 import pandas as pd
+from SqlProvider import SqlProvider
 
+
+sqlProvider = SqlProvider()
+sqlProvider.create_tables()
 start_date = datetime(2022, 9, 2)
 sim = Files(start_date)
 
 for file_id in sim:
-    time.sleep(60)
+    time.sleep(300)
     if id is not None:
         download_url = f"https://drive.google.com/uc?id={file_id}"
         df = pd.read_csv(download_url)
-        print(df.count())
+        df.to_sql('Entries', con=sqlProvider.engine, if_exists='append', index=False)
