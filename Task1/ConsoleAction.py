e2ed036cbce377348917b9a18ab4c18fa9d21627
AbedTakriti <abdulrahman.takriti@gmail.com>
2022-10-16 22:25:49 +0300

    Console application

--- Task1/ConsoleAction.py
+++ Task1/ConsoleAction.py
@@ -1,11 +1,12 @@
 import sys
-
+from datetime import time
 import jsonpickle
 
-from Exceptions import UniversityNotFoundException
+from Exceptions import UniversityNotFoundException, ActivitiesOverlapException, ActivityOutOfRangeException
 from Task1.Model.EdInstitution import EdInstitution
 from Utils import get_institution, print_summary
 from Model.Room import Klassroom, LectureAuditorium
+from Model.Activity import Activity
 import re
 
 
@@ -47,9 +48,94 @@ class AddAuditorium(ConsoleAction):
             print(repr(error))
 
 
-# class AddActivityKlassRoom(ConsoleAction):
-#
-# class AddActivityAuditorium(ConsoleAction):
+
+work_start = time.fromisoformat("06:00")
+work_finish =time.fromisoformat("21:00")
+
+class AddActivityKlassRoom(ConsoleAction):
+
+    def execute(self, institutions):
+        try:
+
+            print("Enter institution name :")
+            inst = get_institution(institutions)
+
+
+            print("enter the desired klassroom number :")
+            activity_room = str(input())
+
+            k_room= inst.get_klassroom(activity_room)
+
+            print("enter activity name:")
+            activity_name = str(input())
+
+            print("Enter the slot that you desire in format HH:MM - HH:MM :")
+            slot = re.findall("\d\d[:]\d\d", input())
+
+
+
+            start_slot = time.fromisoformat(slot[0])
+            finish_slot = time.fromisoformat(slot[1])
+
+
+
+
+            activity = Activity(name=activity_name, room=k_room.get_number(), start=start_slot, end=finish_slot)
+
+
+            k_room.add_activity(activity)
+
+            print(f"Successfully added activity to Klassroom from {start_slot} to {finish_slot} ")
+
+        except (ActivityOutOfRangeException) as out_of_range:
+            print(repr(out_of_range))
+        except(ActivitiesOverlapException) as overlap:
+            print(repr(overlap))
+        except (UniversityNotFoundException) as not_found:
+            print(repr(not_found))
+
+
+
+class AddActivityAuditorium(ConsoleAction):
+    def execute(self, institutions):
+        try:
+
+            print("Enter institution name :")
+            inst = get_institution(institutions)
+
+            print("enter the desired Auditorium number :")
+            activity_auditorium = str(input())
+
+            aud_room= inst.get_auditorium(activity_auditorium)
+
+            print("enter activity name:")
+            activity_name = str(input())
+
+            print("Enter the slot that you desire in format HH:MM - HH:MM :")
+            slot = re.findall("\d\d[:]\d\d", input())
+            start_slot = time.fromisoformat(slot[0])
+            finish_slot = time.fromisoformat(slot[1])
+
+            activity = Activity(name=activity_name, room=aud_room.get_number(), start=start_slot, end=finish_slot)
+
+            aud_room.add_activity(activity)
+
+            print(f"Successfully added activity to Klassroom from {start_slot} to {finish_slot} ")
+
+
+
+        except (ActivityOutOfRangeException) as out_of_range:
+
+            print(repr(out_of_range))
+
+        except(ActivitiesOverlapException) as overlap:
+
+            print(repr(overlap))
+
+        except (UniversityNotFoundException) as not_found:
+
+            print(repr(not_found))
+
 
 class Dump(ConsoleAction):
     def execute(self, institutions: list[EdInstitution]) -> None:
