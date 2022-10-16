d0c3c5870f62e4e05505cbfdf80eae57b741c898
Ozzie_kins <nenubariozioma@gmail.com>
2022-10-16 15:02:25 +0300

    ml to ui

--- ui.py
+++ ui.py
@@ -4,6 +4,7 @@ from src import Entry
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
+from datetime import datetime, date, timedelta
 
 sqlProvider = SqlProvider()
 
@@ -14,6 +15,17 @@ def statsFor7Days():
     # if today is earlier than start day - 7days, then print just stats for all the days since start with a note
     df = pd.read_sql_query('select * from "Entries";', con=sqlProvider.engine)
     df["timestamp"] = pd.to_datetime(df["timestamp"])
+
+    df.sort_values(by="timestamp")
+    df["just_date"] = df["timestamp"].dt.date
+    today = df["just_date"].iat[0]
+    week_prior = today - timedelta(weeks=1)
+    first_day = date(2022, 9, 1)
+
+    num_days = today - first_day
+
+    if week_prior < first_day:
+        print(f"It has only been {num_days} since we started our server. However, we will send the report for this period.")
 
     total_sessions = df.session_id.nunique()
 
@@ -46,13 +58,13 @@ def printUserSummary():
     # 07393db8-e059-4cbd-b16f-88ab2019f045
     userID = input("Enter user id: \n")
 
-    df = pd.read_sql_query('select * from "Entries";', con=sqlProvider.engine)
+    df = pd.read_sql_query(f"""select * from public."Entries" where client_user_id='{userID}';""", con=sqlProvider.engine)
 
     while True:
         if userID in set(df['client_user_id']):
             df["timestamp"] = pd.to_datetime(df["timestamp"])
 
-            df.query(f"client_user_id == '{userID}'", inplace=True)
+            # df.query(f"client_user_id == '{userID}'", inplace=True)
             num_sessions = df.session_id.nunique()
 
             df.sort_values(by="timestamp")
@@ -61,7 +73,7 @@ def printUserSummary():
 
             sessions_time = df.groupby("session_id")["timestamp"].agg(["max", "min"])
             sessions_time["diff"] = sessions_time["max"] - sessions_time["min"]
-            average_per_session = sessions_time["diff"].mean() / pd.Timedelta('1 minute')
+            average_per_session = round(sessions_time["diff"].mean() / pd.Timedelta('1 minute'), 2)
 
             df.sort_values(by="timestamp", ascending=False)
             recent_session = df["just_date"].iat[0]
@@ -69,6 +81,10 @@ def printUserSummary():
             most_device = df["device"].mode().values
 
             devices = df["device"].unique()
+
+            num_bad_sesssions = totalNumberOfBadSessions(userID, df)
+
+            estimated_next_session_time = predictNextSessionDuration(userID)
 
             super_user = isSuperUser(userID, df)
 
@@ -89,14 +105,14 @@ def printUserSummary():
                     Most frequently used device : {most_device}
                     Devices used : {devices}
                     Average of : 1) Round trip time is {rtt} (RTT) 2) Frames per Second is {fps} 3) Dropped Frames is {dropped_frames} 4) bitrate is {bitrate}
-                    Total number of bad sessions (predicted using ML model)
-                    Estimated next session time : 4 hrs
+                    Total number of bad sessions (predicted using ML model): {num_bad_sesssions}
+                    Estimated next session time : {estimated_next_session_time} hrs
                     Super user : {super_user}\n""")
 
             answer = input("Find another user ? (yes/no) \n")
             if answer == "yes":
                 userID = input("Enter user id: \n")
-                df = pd.read_sql_query('select * from "Entries";', con=sqlProvider.engine)
+                df = pd.read_sql_query(f"""select * from public."Entries" where client_user_id='{userID}';""", con=sqlProvider.engine)
             elif answer == "no":
                 break
             else:
@@ -110,8 +126,64 @@ def printUserSummary():
 #     # b3aebc80-ff28-4569-bd18-2ace692f668e
 #     sessionID = input("Enter session id: \n")
 
-def predictNextSessionDuration():
-    pass
+def totalNumberOfBadSessions(userID, df):
+
+    df = df.groupby("session_id")["timestamp"].agg(["max", "min"])
+    agg = {
+        'fps': ['mean', 'std'],
+        'RTT': ['mean', 'std'],
+        'dropped_frames': ['mean', 'std', 'max']
+    }
+    aggregate_df = df.groupby('session_id', as_index=False).agg(agg)
+    aggregate_df.columns = ['session_id', 'FPS_mean', 'FPS_std', 'RTT_mean', 'RTT_std', 'dropped_frames_mean',
+                            'dropped_frames_std', 'dropped_frames_max']
+
+    # qualityPredictor = QualityPredictor()
+    # num = qualityPredictor.predict(aggregate_df)
+    #
+    # return num
+
+def predictNextSessionDuration(userID):
+
+    df = pd.read_sql_query(f"""select * from public."AggregateEntries" where client_user_id='{userID}';""", con=sqlProvider.engine)
+
+    agg = {
+        'dropped_frames_mean': ['min', 'mean'],
+        'FPS': ['min', 'max', 'mean', 'std'],
+        'RTT': ['min', 'max', 'mean', 'std'],
+        'bitrate': ['min', 'max', 'mean', 'std']
+    }
+    aggregate_df = df.groupby('session_id', as_index=False).agg(agg)
+    aggregate_df.columns = ['session_id', 'dropped_frames_min', 'dropped_frames_mean',
+                            'FPS_min', 'FPS_max', 'FPS_mean', 'FPS_std', 'RTT_min', 'RTT_max',
+                            'RTT_mean', 'RTT_std', 'bitrate_min', 'bitrate_max', 'bitrate_mean', 'bitrate_std']
+
+    dropped_frames_min = df["dropped_frames_min"].mean()
+    dropped_frames_mean = df["dropped_frames_mean"].mean()
+
+    fps_min = df["FPS_min"].mean()
+    fps_max = df["FPS_max"].mean()
+    fps_mean = df["FPS_mean"].mean()
+    fps_std = df["FPS_std"].mean()
+
+    rtt_min = df["RTT_min"].mean()
+    rtt_max = df["RTT_max"].mean()
+    rtt_mean = df["RTT_mean"].mean()
+    rtt_std = df["RTT_std"].mean()
+
+    bitrate_min = df["bitrate_min"].mean()
+    bitrate_max = df["bitrate_max"].mean()
+    bitrate_mean = df["bitrate_mean"].mean()
+    bitrate_std = df["bitrate_std"].mean()
+
+    data = [dropped_frames_min, dropped_frames_mean,
+            fps_min, fps_max, fps_mean, fps_std,
+            rtt_min, rtt_max, rtt_mean, rtt_std,
+            bitrate_min, bitrate_max, bitrate_mean, bitrate_std]
+
+    # next_session_duration = DurationPredict(data)
+    # return next_session_duration
+    return data
 
 def fetchAndUpdateData():
     pass
@@ -124,8 +196,10 @@ def topFiveUsers():
     sessions_time = df.groupby(["client_user_id", "session_id"])["timestamp"].agg(["max", "min"])
     sessions_time["diff"] = sessions_time["max"] - sessions_time["min"]
     sessions_time_total = sessions_time.groupby("client_user_id")["diff"].agg(["sum"])
-    sessions_time_total["hours spent gaming"] = sessions_time_total["sum"].dt.components["hours"]
+    sessions_time_total["days_spent_gaming"] = sessions_time_total["sum"].dt.components["days"]
+    sessions_time_total["hours_spent_gaming"] = sessions_time_total["sum"].dt.components["hours"]
     sessions_time_total = sessions_time_total.sort_values(by="sum", ascending=False)
+    sessions_time_total = sessions_time_total.drop(columns=["sum"])
 
     top_users = sessions_time_total.head(5)
     print("Top users are: \n", top_users, "\n")
