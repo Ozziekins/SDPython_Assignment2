661d8e42858e1fca3f4a4b03da6d6200d18b2f36
Pain122 <pavelx6@gmail.com>
2022-10-17 18:34:21 +0300

    Recomment the stuff

--- Task1/main.py
+++ Task1/main.py
@@ -3,27 +3,15 @@ from Task1.ConsoleAction import *
 from Task1.Model.EdInstitution import EdInstitution
 
 if __name__ == '__main__':
-    # innopolis = EdInstitution('Innopolis University')
-    # auditorium = LectureAuditorium('KFU', '494', 120, False)
-    # activity = Activity("sas", '494', time.fromisoformat("10:30"), time.fromisoformat("11:30"))
-    # auditorium.add_activity(activity)
-    # kfu = EdInstitution('KFU')
-    # kfu.add_auditorium(auditorium)
-    # institutions = [innopolis, kfu]
-    # actions = [AddAuditorium(), PrintSummary(), AddActivityKlassRoom(), AddActivityAuditorium(), Dump(), Exit()]
-    # console = Console(actions, institutions)
-    #
-    # while True:
-    #     console.choose_action()
-
-    act = Activity("sas", '494', time.fromisoformat("10:30"), time.fromisoformat("11:30"))
-    act2 = Activity("sas", '494', time.fromisoformat("10:30"), time.fromisoformat("11:30"))
-
-    auditorium = LectureAuditorium('KFU', '494', 120, False)
-    auditorium.add_activity(act)
-
     innopolis = EdInstitution('Innopolis University')
+    auditorium = LectureAuditorium('KFU', '494', 120, False)
+    activity = Activity("sas", '494', time.fromisoformat("10:30"), time.fromisoformat("11:30"))
+    auditorium.add_activity(activity)
+    kfu = EdInstitution('KFU')
+    kfu.add_auditorium(auditorium)
+    institutions = [innopolis, kfu]
+    actions = [AddAuditorium(), PrintSummary(), AddActivityKlassRoom(), AddActivityAuditorium(), Dump(), Exit()]
+    console = Console(actions, institutions)
 
-    innopolis.add_auditorium(auditorium)
-
-    print(innopolis)
+    while True:
+        console.choose_action()
